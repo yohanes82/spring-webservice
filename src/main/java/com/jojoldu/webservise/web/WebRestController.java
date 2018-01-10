8440ff8890017cf4f811e1d9ddbae238b0e8b967
운영 <yohanes82@naver.com>
2018-01-10 16:23:50 +0900

    프로젝트 생성

---
+++ src/main/java/com/jojoldu/webservise/web/WebRestController.java
@@ -0,0 +1,12 @@
+package com.jojoldu.webservise.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebRestController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "Helloworld";
+    }
+}
